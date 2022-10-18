dfc691c79c95c9591016295a6ae0d5221f70a2e7
WiiruxCruz <hialf.92@gmail.com>
2022-10-17 23:47:43 -0500

    Implement get author by id

--- src/main/java/guru/springframework/jdbc/dao/impl/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/impl/AuthorDaoImpl.java
@@ -1,17 +1,26 @@
 package guru.springframework.jdbc.dao.impl;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import guru.springframework.jdbc.dao.AuthorDao;
 import guru.springframework.jdbc.dao.mapper.AuthorMapper;
 import guru.springframework.jdbc.domain.Author;
 
+@Component
 public class AuthorDaoImpl implements AuthorDao {
-
+	
+	private final JdbcTemplate jdbcTemplate;
+	
+	public AuthorDaoImpl(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
 	@Override
 	public Author getById(Long id) {
 		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.queryForObject("SELECT * FROM author where id = ?", getRowMapper(), id);
 	}
 
 	@Override
